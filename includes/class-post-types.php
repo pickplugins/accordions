494fd0c3562e158c7ce76fcdba9594030815dd40
Nur Hasan <32812881+pickplugins@users.noreply.github.com>
2023-11-23 11:04:53 +0600

    version update

--- includes/class-post-types.php
+++ includes/class-post-types.php
@@ -57,7 +57,7 @@ class accordions_post_types
 				'hierarchical' 			=> false,
 				'rewrite' 				=> true,
 				'query_var' 			=> true,
-				'supports' 				=> array('title',),
+				'supports' 				=> array('title', 'revisions'),
 				'show_in_nav_menus' 	=> true,
 				//'show_in_menu' 	=> 'edit.php?post_type=team',	
 				'menu_icon' => 'dashicons-align-center',
