97ab4d7670acb19977f78e03ec9c4fefa673b2b2
Nur <support@pickplugins.com>
2024-06-09 20:57:48 +0600

    update version

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-keycodes', 'wp-primitives'), 'version' => '84fbdeae78a42d82b757');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-keycodes', 'wp-primitives'), 'version' => '8a606ed02f8de8826ea0');
