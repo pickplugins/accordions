9f139523733ffebd0e638d0e93c57b59b4fe1bd2
Nur <support@pickplugins.com>
2025-01-04 22:35:55 +0600

    update version

--- includes/class-post-types.php
+++ includes/class-post-types.php
@@ -109,7 +109,7 @@ class accordions_post_types
 				'hierarchical' 			=> false,
 				'rewrite' 				=> true,
 				'query_var' 			=> true,
-				'supports' 				=> array('title', 'editor', 'revisions'),
+				'supports' 				=> array('title',  'revisions'),
 				'show_in_nav_menus' 	=> true,
 				//'show_in_menu' 	=> 'edit.php?post_type=team',	
 				'menu_icon' => 'dashicons-align-center',
